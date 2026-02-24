2866065a6134137857f4c9a1b30b7bd97c1499b7
D. Paolella <dpaolell@redhat.com>
2026-02-24 17:27:31 +0100

    Fix images-health dashboard filters and unify OKD/OCP dashboard

--- pyartcd/pyartcd/pipelines/okd_images_health.py
+++ pyartcd/pyartcd/pipelines/okd_images_health.py
@@ -1,19 +1,19 @@
 import asyncio
 import json
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 from typing import Optional
 from urllib.parse import quote
 
 import click
 from artcommonlib import exectools, redis
 from doozerlib.cli.images_health import DELTA_DAYS, LIMIT_BUILD_RESULTS, ConcernCode
+from doozerlib.constants import ART_BUILD_HISTORY_URL
 
 from pyartcd.cli import cli, click_coroutine, pass_runtime
 from pyartcd.constants import OCP_BUILD_DATA_URL, OKD_ENABLED_VERSIONS
 from pyartcd.runtime import Runtime
 
 OKD_GROUP_TEMPLATE = "okd-{}"
-OKD_BUILD_HISTORY_URL = 'https://okd-build-history-okd-build-history.apps.artc2023.pc3z.p1.openshiftapps.com'
 
 
 class ImagesHealthPipeline:
@@ -242,7 +242,9 @@ class ImagesHealthPipeline:
         group = concern['group']
         # Transform openshift-X.Y to okd-X.Y for the OKD dashboard
         okd_group = group.replace('openshift-', 'okd-')
-        return f'{OKD_BUILD_HISTORY_URL}/?name=^{image_name}$&group={okd_group}'
+        start_date = (datetime.now(timezone.utc) - timedelta(days=DELTA_DAYS)).strftime('%Y-%m-%d')
+        end_date = datetime.now(timezone.utc).strftime('%Y-%m-%d')
+        return f'{ART_BUILD_HISTORY_URL}/?name=^{image_name}$&group={okd_group}&assembly=stream&engine=konflux&dateRange={start_date}+to+{end_date}&outcome=success&outcome=failure'
 
     @staticmethod
     def get_logs_url(concern):
@@ -256,7 +258,7 @@ class ImagesHealthPipeline:
         """
         dt = datetime.fromisoformat(concern['latest_failed_build_time'])
         formatted = dt.astimezone(timezone.utc).strftime("%a, %d %b %Y %H:%M:%S GMT")
-        logs_url = f'{OKD_BUILD_HISTORY_URL}/logs?nvr={concern["latest_failed_nvr"]}&record_id={concern["latest_failed_build_record_id"]}&after={formatted}'
+        logs_url = f'{ART_BUILD_HISTORY_URL}/logs?nvr={concern["latest_failed_nvr"]}&record_id={concern["latest_failed_build_record_id"]}&after={formatted}'
         return logs_url
 
     @staticmethod
